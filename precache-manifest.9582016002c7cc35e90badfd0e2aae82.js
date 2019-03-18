db3bc4b7df93e8e0bcaadf165612b555e0f659fe
Andreas Otten <aotten77@googlemail.com>
2019-03-18 22:54:07 +0100

    deploy

---
+++ precache-manifest.9582016002c7cc35e90badfd0e2aae82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "26fee0059ec098d5f609",
+    "url": "/js/about.d288b4f1.js"
+  },
+  {
+    "revision": "fc3e84dc9e60379520b6",
+    "url": "/css/app.41afe0d7.css"
+  },
+  {
+    "revision": "fc3e84dc9e60379520b6",
+    "url": "/js/app.b7a4088e.js"
+  },
+  {
+    "revision": "b481acc781da09280082",
+    "url": "/js/chunk-vendors.2e4ab3f2.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "db07d0d426543569636be144dfcfaca5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
